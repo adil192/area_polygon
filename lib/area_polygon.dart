a66cbedc66e735aacafb840c56b71cc7f3e78eb3
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-10-10 18:40:10 +0100

    feat: port math-utils/area-polygon

--- lib/area_polygon.dart
+++ lib/area_polygon.dart
@@ -1,7 +1,32 @@
-library area_polygon;
+import 'dart:ui';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// Calculate the area of a simple 2D polygon given its vertices.
+///
+/// The [points] must trace the edge of the polygon.
+/// The last point is assumed to be connected to the first point.
+///
+/// If [signed] is true, this will return the signed area.
+/// This is helpful to determine the orientation of [points].
+///
+/// Signed area is positive if the [points] are counter-clockwise,
+/// or negative if the [points] are clockwise.
+double calculateArea(
+  List<Offset> points, {
+  bool signed = false,
+}) {
+  double det = 0;
+
+  if (points.first != points.last) {
+    points = List.from(points)..add(points.first);
+  }
+
+  for (int i = 0; i < points.length - 1; ++i) {
+    det += points[i].dx * points[i + 1].dy - points[i].dy * points[i + 1].dx;
+  }
+
+  if (signed) {
+    return det / 2;
+  } else {
+    return det.abs() / 2;
+  }
 }
--- test/area_polygon_test.dart
+++ test/area_polygon_test.dart
@@ -3,10 +3,92 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:area_polygon/area_polygon.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('Calculate area of', () {
+    test('1x1 square', () {
+      const polygon = [
+        Offset(0, 0),
+        Offset(0, 1),
+        Offset(1, 1),
+        Offset(1, 0),
+      ];
+      expect(calculateArea(polygon), 1);
+    });
+
+    test('1x1 square (reverse)', () {
+      const polygon = [
+        Offset(1, 0),
+        Offset(1, 1),
+        Offset(0, 1),
+        Offset(0, 0),
+      ];
+      expect(calculateArea(polygon), 1);
+    });
+
+    test('2x4 rectangle', () {
+      const polygon = [
+        Offset(0, 0),
+        Offset(2, 0),
+        Offset(2, 4),
+        Offset(0, 4),
+      ];
+      expect(calculateArea(polygon), 8);
+    });
+
+    test('3x4 right triangle', () {
+      const polygon = [
+        Offset(0, 0),
+        Offset(3, 0),
+        Offset(3, 4),
+      ];
+      expect(calculateArea(polygon), 6);
+    });
+
+    test('anti-clockwise concave polygon (signed)', () {
+      const polygon = [
+        Offset(2, 4),
+        Offset(7, 0),
+        Offset(18, 9),
+        Offset(16, 13),
+        Offset(14, 10),
+        Offset(11, 8),
+        Offset(8, 11),
+        Offset(7, 15),
+        Offset(4, 11),
+        Offset(2, 4),
+      ];
+      expect(calculateArea(polygon, signed: true), 108);
+    });
+
+    test('clockwise concave polygon (signed)', () {
+      const polygon = [
+        Offset(2, 4),
+        Offset(4, 11),
+        Offset(7, 15),
+        Offset(8, 11),
+        Offset(11, 8),
+        Offset(14, 10),
+        Offset(16, 13),
+        Offset(18, 9),
+        Offset(7, 0),
+        Offset(2, 4),
+      ];
+      expect(calculateArea(polygon, signed: true), -108);
+    });
+
+    test('clockwise concave polygon (unsigned)', () {
+      const polygon = [
+        Offset(2, 4),
+        Offset(4, 11),
+        Offset(7, 15),
+        Offset(8, 11),
+        Offset(11, 8),
+        Offset(14, 10),
+        Offset(16, 13),
+        Offset(18, 9),
+        Offset(7, 0),
+        Offset(2, 4),
+      ];
+      expect(calculateArea(polygon, signed: false), 108);
+    });
   });
 }
